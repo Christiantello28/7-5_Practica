0f8c21706f1837ae923bd8f3c2cd1acebae223bc
Francisco jose Ortuño Torrano <franciscojose.ortuno.torrano@ces-vegamedia.es>
2026-02-24 10:44:55 +0100

    js

--- js/javascript.js
+++ js/javascript.js
@@ -8,7 +8,11 @@
     function tienda(){
 
     }
+    var carta2 = new carta(1,"img/2oros.png");
+    var carta3 = new carta(3,"img/3oros.png");
+    var carta4 = new carta(4,"img/4oros.png");
 
-    var carta1 = new carta(1,)
-
-
+    var arrayCartas= new[carta2,carta3,carta4];
+    
+    var elegido=arraycartas[1];
+    document.getElementById("demo").src=elegido.url;
