858ca91ea982c084a9992352ccbb5e99d3706157
Francisco jose Ortuño Torrano <franciscojose.ortuno.torrano@ces-vegamedia.es>
2026-02-24 09:54:02 +0100

    carpeta img añadida

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,14 @@
  class carta {
         constructor (arg1, arg2) {
             this.valor=parseInt(arg1);
-            this.imagen=toString(arg2);
-        }
-        // Métodos
-        get area() {
-            return this.x * this.y;
-         }
-        get volumen () {
-            return this.x * this.y * this.z;
+            this.url=(arg2);
         }
       }
+
+    function tienda(){
+
+    }
+
+    var carta1 = new carta(1,)
+
+
