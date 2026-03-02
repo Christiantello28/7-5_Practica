246e5b80978ca0e3a351fcab160d8f8856ae7578
Francisco jose Ortuño Torrano <franciscojose.ortuno.torrano@ces-vegamedia.es>
2026-03-02 14:16:02 +0100

    todo cambiado sistema de banca añadido

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
  
  class carta {
         constructor (arg1, arg2) {
-            this.valor=parseInt(arg1);
+            this.valor=parseFloat(arg1);
             this.url=(arg2);
         }
       }
@@ -18,9 +18,9 @@
     var carta5 = new carta(5,"img/5oros.png");
     var carta6 = new carta(6,"img/6oros.png");
     var carta7 = new carta(7,"img/7oros.png");
-    var carta8 = new carta(10,"img/sotaoros.png");
-    var carta9 = new carta(11,"img/caballooros.png");
-    var carta10 = new carta(12,"img/reyoros.png");
+    var carta8 = new carta(0.5,"img/sotaoros.png");
+    var carta9 = new carta(0.5,"img/caballooros.png");
+    var carta10 = new carta(0.5,"img/reyoros.png");
     //copas (11-20)
     var carta11 = new carta(1,"img/1copas.png");
     var carta12 = new carta(2,"img/2copas.png");
@@ -29,9 +29,9 @@
     var carta15 = new carta(5,"img/5copas.png");
     var carta16 = new carta(6,"img/6copas.png");
     var carta17 = new carta(7,"img/7copas.png");
-    var carta18 = new carta(10,"img/sotacopas.png");
-    var carta19 = new carta(11,"img/caballocopas.png");
-    var carta20 = new carta(12,"img/reycopas.png");
+    var carta18 = new carta(0.5,"img/sotacopas.png");
+    var carta19 = new carta(0.5,"img/caballocopas.png");
+    var carta20 = new carta(0.5,"img/reycopas.png");
     //espadas (21-30)
     var carta21 = new carta(1,"img/1espada.png");
     var carta22 = new carta(2,"img/2espada.png");
@@ -40,9 +40,9 @@
     var carta25 = new carta(5,"img/5espada.png");
     var carta26 = new carta(6,"img/6espada.png");
     var carta27 = new carta(7,"img/7espada.png");
-    var carta28 = new carta(10,"img/sotaespada.png");
-    var carta29 = new carta(11,"img/caballoespada.png");
-    var carta30 = new carta(12,"img/reyespada.png");
+    var carta28 = new carta(0.5,"img/sotaespada.png");
+    var carta29 = new carta(0.5,"img/caballoespada.png");
+    var carta30 = new carta(0.5,"img/reyespada.png");
     //bastos (31-40)
     var carta31 = new carta(1,"img/1bastos.png");
     var carta32 = new carta(2,"img/2bastos.png");
@@ -51,9 +51,9 @@
     var carta35 = new carta(5,"img/5bastos.png");
     var carta36 = new carta(6,"img/6bastos.png");
     var carta37 = new carta(7,"img/7bastos.png");
-    var carta38 = new carta(10,"img/sotabastos.png");
-    var carta39 = new carta(11,"img/caballobastos.png");
-    var carta40 = new carta(12,"img/reybastos.png");
+    var carta38 = new carta(0.5,"img/sotabastos.png");
+    var carta39 = new carta(0.5,"img/caballobastos.png");
+    var carta40 = new carta(0.5,"img/reybastos.png");
 
     var arrayCartas=[carta1,carta2,carta3,carta4,carta5,carta6,carta7,carta8,carta9,carta10,
                     carta11,carta12,carta13,carta14,carta15,carta16,carta17,carta18,carta19,carta20,
@@ -61,37 +61,42 @@
                     carta31,carta32,carta33,carta34,carta35,carta36,carta37,carta38,carta39,carta40];
 
     let plantarse=false;
-    let valor=0;
+    let valor=0.0;
+
     function sacarcarta(){
-      valor=new carta();
-      console.log(valor);
-      if(valor<=7.5 || plantarse==false){
+     
+      if(valor<=7.5 && plantarse==false){
         var nuevaImagen = document.createElement("img");
   
         var random=Math.floor(Math.random()*arrayCartas.length)
         var elegido=arrayCartas[random];
         nuevaImagen.src=elegido.url;
-  
+
+        valor+=parseFloat(elegido.valor);
+        console.log("jugador: "+valor);
+
         arrayCartas.splice(random, 1); 
-        console.log(arrayCartas.length);
         document.getElementById("tapete").appendChild(nuevaImagen);
+      }else{
+
       }
-
     }
-
-    function Pantarse(){
-
+    let valorB=0.0;
+    function Plantarse(){
       plantarse=true;
-      for(let i=0; i<valor; i++){
+      while(valorB<valor){
         var nuevaImagen = document.createElement("img");
     
           var random=Math.floor(Math.random()*arrayCartas.length)
           var elegido=arrayCartas[random];
           nuevaImagen.src=elegido.url;
-    
+
+          valorB+=parseFloat(elegido.valor);
+          console.log("banca: "+valorB); 
+
           arrayCartas.splice(random, 1); 
           console.log(arrayCartas.length);
-          document.getElementById("tapete").appendChild(nuevaImagen);
+          document.getElementById("banca").appendChild(nuevaImagen);
 
       }
     }
